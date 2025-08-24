54c724d03f65cfddc3504488135a1563c4ee220b
Priyam Bhattacharya <itspriyambhattacharya@gmail.com>
2025-08-24 23:06:39 +0530

    Refactor strings_prac.py to enhance string manipulation examples and add conversion methods

--- 01_basics/strings_prac.py
+++ 01_basics/strings_prac.py
@@ -27,3 +27,22 @@ print(s4.replace("go", "don't go"))
 s4 = "0,1,2,3,4,5,6,7,8,9,10"
 s5 = s4.split(",")
 print(s5)
+
+print("Good weather weather".find("weather"))
+s4 = "let's go go go"
+print(s4.count("go"))
+
+name = 'Priyam Bhattacharya'
+age = 24
+
+print("My name is {} and age is {}years".format(name, age))
+print(f"My name is {name} and age is {age}years.")
+
+# Converting string to lisst
+s1 = "0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10"
+ls1 = s1.split(", ")
+print(ls1)
+s2 = "".join(ls1)  # basic joining
+print(s2)
+s2 = ", ".join(ls1)  # basic joining
+print(s2)
