9ecaac513c685941c454ef31f36b2a7facbe3842
Priyam Bhattacharya <itspriyambhattacharya@gmail.com>
2025-08-24 14:17:25 +0530

    Add Decimal calculations to numbers_prac.py for accurate floating-point arithmetic

--- 01_basics/numbers_prac.py
+++ 01_basics/numbers_prac.py
@@ -1,3 +1,4 @@
+from decimal import Decimal
 n = 1
 print(n << 2)
 
@@ -9,3 +10,7 @@ x = 0.1
 y = 0.3
 res = x+x+x-y  # problem
 print(res)
+
+y = Decimal('0.1')+Decimal('0.1')+Decimal('0.1')-Decimal('0.3')
+print(y)
+print(type(y))
