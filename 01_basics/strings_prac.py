ed5aa07c722c777ca1efb8900a9bd5e5d771753d
Priyam Bhattacharya <itspriyambhattacharya@gmail.com>
2025-08-24 20:19:26 +0530

    Enhance strings_prac.py with additional string slicing and manipulation examples

--- 01_basics/strings_prac.py
+++ 01_basics/strings_prac.py
@@ -1,2 +1,29 @@
 s1 = '0123456789'
 # string slicing
+print(s1[:])
+print(s1[1:5])
+print(s1[1:1])
+print(s1[1:2])
+print(s1[6:1])
+print(s1[-6:-1])
+print(s1[2:8:1])
+print(s1[2:8:2])
+print(s1[-8:-2:2])
+
+
+s2 = "hello world     "
+print("Length of s2 is ", len(s2))
+print(s2.lower())
+print(s2.upper())
+
+s3 = s2.strip()  # similar to trim
+print(s3, len(s3))
+
+s3 = "let's go"
+s4 = "let's go go go"
+print(s3.replace("go", "don't go"))
+print(s4.replace("go", "don't go"))
+
+s4 = "0,1,2,3,4,5,6,7,8,9,10"
+s5 = s4.split(",")
+print(s5)
