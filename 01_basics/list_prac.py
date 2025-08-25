7ff7f394f3a81edcd4b2b494ccc228ef10492258
Priyam Bhattacharya <itspriyambhattacharya@gmail.com>
2025-08-25 15:07:05 +0530

    Enhance list_prac.py with additional examples of list slicing and manipulation

--- 01_basics/list_prac.py
+++ 01_basics/list_prac.py
@@ -1,2 +1,24 @@
 l1 = ["Hello", 'Priyam', 'world', 'go', 'great', 'hello world', 'bhattacharya']
 print(l1)
+print(l1[1:4])
+l1[1:2] = "hello2"  # problem
+print(l1)
+l1 = ["Hello", 'Priyam', 'world', 'go', 'great', 'hello world', 'bhattacharya']
+l1[1:2] = ["hello2"]  # solution
+print(l1)
+
+l1[1:3] = ['hello5', 'priyam5']
+print(l1)
+
+lst = [1, 2, 3, 4, 5, 6]
+lst[1:3] = [22, 33]
+print(lst)
+
+print(lst[1:1])
+lst[1:1] = ["t1", "t2"]
+print(lst)
+
+lst = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+lst[1:3] = []
+print(lst)
