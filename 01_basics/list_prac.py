03f6a5568796b37ffd04b46cb0c4f89e475ccf2a
Priyam Bhattacharya <itspriyambhattacharya@gmail.com>
2025-08-25 16:00:02 +0530

    Add examples of list manipulation methods to list_prac.py

--- 01_basics/list_prac.py
+++ 01_basics/list_prac.py
@@ -37,3 +37,30 @@ if 34 in lst:
     print("Yes exists")
 else:
     print("No, doesn't exists")
+
+
+lst = [1, 2, 3, 4]
+print(lst)
+lst.append(55)
+print(lst)
+x = lst.pop()
+print(f"x is {x} and lst is {lst}")
+y = lst.remove(4)
+print(f"y is {y} and lst is {lst}")
+print(lst)
+
+lst2 = lst  # same memory location is shared between lst and lst2
+print("lst2 is", lst2)
+lst.insert(3, 77)
+print("Inserted successfully")
+print(f"lst is {lst}")
+print(f"lst2 is {lst2}")
+# THis problem can be solved in the following way
+
+print(lst)
+lst2copy = lst.copy()  # separate memory location is created
+lst.pop()
+print(lst)
+print(lst2copy)
+lst.insert(88, 100)
+print(lst)
