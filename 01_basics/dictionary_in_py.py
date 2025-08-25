e26544f417370592a527b4d8fbaeb3d25e9e6852
Priyam Bhattacharya <itspriyambhattacharya@gmail.com>
2025-08-25 19:45:20 +0530

    Add examples of nested dictionaries and retrieval methods to dictionary_in_py.py

--- 01_basics/dictionary_in_py.py
+++ 01_basics/dictionary_in_py.py
@@ -12,3 +12,22 @@ print(f"The first name is {myDict1['first name']}")
 
 myDict1['age'] = 25
 print(f"The dictionary is {myDict1}")
+
+# nested dictionary
+print('================================================ Nested Dictionary ================================================')
+tea_shop = {
+    "tea1": {
+        "masala": "spicy", "ginger": "zesty"
+    },
+    "tea2": {
+        "green": "mild",
+        "black": "strong"
+    }
+}
+
+print(tea_shop)
+
+print(tea_shop["tea1"])  # return a sub-dictionary
+print(tea_shop["tea2"]["black"])
+print(tea_shop.get("tea2"))
+print(tea_shop.get("tea2").get("green"))
