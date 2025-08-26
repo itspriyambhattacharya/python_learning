320b26ffcc7739fbf2368da3f770116c96b23d53
Priyam Bhattacharya <itspriyambhattacharya@gmail.com>
2025-08-26 22:21:57 +0530

    multiply numbers and string

---
+++ 05_functions/03_solution.py
@@ -0,0 +1,11 @@
+def multiply(a, b):
+    return a*b
+
+
+n1 = int(input("Enter a number: "))
+n2 = int(input("Enter another number: "))
+res = multiply(n1, n2)
+print(res)
+
+n3 = input("Enter a string: ")
+print(multiply(n1, n3))
