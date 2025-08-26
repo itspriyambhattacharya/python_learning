ee8edcfa4e7d0c30a4e6bf19c712e9fc7245ccfd
Priyam Bhattacharya <itspriyambhattacharya@gmail.com>
2025-08-26 12:58:49 +0530

    Fix logic to correctly handle the case when no non-repeating character is found in the second method

--- 3_loops/loop_q6.py
+++ 3_loops/loop_q6.py
@@ -14,7 +14,11 @@ else:
 
 
 # Method 2(Eaasier one):
+fl = 0
 for c in s1:
     if (s1.count(c) == 1):
         print(f"The first non-repeating character is {s1[idx]}")
+        fl = 1
         break
+if fl == 0:
+    print("Nothing found")
