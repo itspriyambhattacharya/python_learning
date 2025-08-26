fbdf6580cac5dc56d51506887150fe88179a79e4
Priyam Bhattacharya <itspriyambhattacharya@gmail.com>
2025-08-26 13:12:53 +0530

    Replace string processing code with factorial calculation implementation

--- 3_loops/loop_q6.py
+++ 3_loops/loop_q6.py
@@ -1,24 +1,10 @@
-s1 = input("Enter a string: ").strip()
-size = len(s1)
-fl = False
-idx = 0
-for i in range(0, size, 1):
-    if ((s1[i] not in s1[0: i]) and (s1[i] not in s1[i+1:])):
-        fl = True
-        idx = i
-        break
-if fl == True:
-    print(f"The first non-repeating character is {s1[idx]}")
-else:
-    print("Nothing found")
-
-
-# Method 2(Eaasier one):
-fl = 0
-for c in s1:
-    if (s1.count(c) == 1):
-        print(f"The first non-repeating character is {c}")
-        fl = 1
-        break
-if fl == 0:
-    print("Nothing found")
+n = int(input("Enter a number: "))
+i = 1
+f = 1
+if n == 0:
+    print("The factorial of 0 is 1")
+    exit()
+while (i <= n):
+    f = f*i
+    i += 1
+print(f"The factorial of {n} is {f}")
