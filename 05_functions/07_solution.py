35be6a017228cf1edb0e0f1b31e1c8742f2ce80f
Priyam Bhattacharya <itspriyambhattacharya@gmail.com>
2025-08-26 23:06:25 +0530

    investigative study on variable-size arguments

--- 05_functions/07_solution.py
+++ 05_functions/07_solution.py
@@ -1,8 +1,13 @@
 def sum_all(*args):
+    print("*args is ", *args)
+    # args in a tuple, an iterable, so we can apple loops and perform other task as well
+    print(f"args is {args}")
+    for i in args:
+        print(i**2)
     return sum(args)
 
 
-print(sum_all(1, 2))
+# print(sum_all(1, 2))
 print(sum_all(1, 2, 3))
-print(sum_all(1, 2, 3, 4))
-print(sum_all(1, 2, 3, 4, 5))
+# print(sum_all(1, 2, 3, 4))
+# print(sum_all(1, 2, 3, 4, 5))
