35d9312f5ae7add66195af263afc3574a03520b6
Priyam Bhattacharya <itspriyambhattacharya@gmail.com>
2025-08-27 23:03:48 +0530

    started learning oops

---
+++ 07_oops/01_solution.py
@@ -0,0 +1,28 @@
+class Car:
+    def __init__(self, brand, model):
+        self.brand = brand
+        self.model = model
+
+    def display(self):
+        return f"{self.brand} {self.model}"
+
+
+class ElectricCar(Car):
+    def __init__(self, brand, model, battery_size):
+        super().__init__(brand, model)
+        self.battery_size = battery_size
+
+    def display(self):
+        return super().display()
+
+
+c1 = Car("Toyota", "Corolla")
+c2 = Car("Tata", "Safari")
+
+print(c1.brand)
+print(c1.model)
+print(c2.model)
+print(c2.display())
+
+c3 = ElectricCar("Tesla", "Model1", "4000")
+print(c3.display())
