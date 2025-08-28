51b1ae808d64e85b0a1869492783b1211bb65680
Priyam Bhattacharya <itspriyambhattacharya@gmail.com>
2025-08-28 21:09:28 +0530

    private access type

--- 07_oops/01_solution.py
+++ 07_oops/01_solution.py
@@ -1,10 +1,16 @@
 class Car:
     def __init__(self, brand, model):
-        self.brand = brand
-        self.model = model
+        self.__brand = brand  # changing the acces type of data members from public to private
+        self.__model = model  # changing the acces type of data members from public to private
 
     def display(self):
-        return f"{self.brand} {self.model}"
+        return f"{self.__brand} {self.__model}"
+
+    def get_brand(self):
+        return self.__brand
+
+    def get_model(self):
+        return self.__model
 
 
 class ElectricCar(Car):
@@ -19,9 +25,14 @@ class ElectricCar(Car):
 c1 = Car("Toyota", "Corolla")
 c2 = Car("Tata", "Safari")
 
+'''
 print(c1.brand)
 print(c1.model)
 print(c2.model)
+'''
+print(c1.get_brand())
+print(c1.get_model())
+print(c2.get_model())
 print(c2.display())
 
 c3 = ElectricCar("Tesla", "Model1", 4000)
